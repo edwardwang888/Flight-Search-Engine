0df28771dff27fe57547ac652be6f135e906cb15
Edward Wang <edwardwang@Edwards-MacBook-Pro-2.local>
2018-06-24 14:52:59 -0700

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,33 @@
 var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
-    var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/white-plane-sky.jpg') {
+myImage.onmouseenter = function() {
 	myImage.setAttribute ('src','images/plane2.jpg');
-    } else {
-	myImage.setAttribute ('src','images/white-plane-sky.jpg');
-    }
+}
+
+myImage.onmouseleave = function() {
+	myImage.setAttribute('src', 'images/white-plane-sky.jpg');
 }
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h4');
+
 function setUserName(myName) {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = 'Welcome, ' + myName + '!';
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Welcome, ' + myName + '!';
 }
 
 function getUserName() {
-    return prompt('Please enter your name.');
+	name = prompt('Please enter your name.');
+	return name;
 }
 
 if(!localStorage.getItem('name')) {
-    setUserName(getUserName());
+	setUserName(getUserName());
 } else {
-    var storedName = localStorage.getItem('name');
-    setUserName(storedName);
+	var storedName = localStorage.getItem('name');
+	setUserName(storedName);
 }
 
 myButton.onclick = function() {
-    setUserName(getUserName());
+	setUserName(getUserName());
 }
